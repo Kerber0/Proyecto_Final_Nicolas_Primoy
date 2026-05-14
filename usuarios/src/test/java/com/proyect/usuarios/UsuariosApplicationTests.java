4a96c2d9b1f12312d49f7cb5f2f9bbd3f9cbe644
Nico <nprimoy@gmail.com>
2026-05-14 10:15:33 +0200

    14/05/2026

---
+++ usuarios/src/test/java/com/proyect/usuarios/UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyect.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
